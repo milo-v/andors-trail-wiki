16cc07c554c1f1ae9d10004fc352c3f4565976a1
Vũ Minh Long <miloygo1811@gmail.com>
2026-07-21 13:19:28 +0700

    Add optimizerWorker.js Web Worker for Phase D search

---
+++ src/workers/optimizerWorker.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-restricted-globals */
+import { buildCandidateLists, searchBestBuilds } from '../utils/combat/optimizer';
+
+let cancelled = false;
+
+self.onmessage = async (event) => {
+    const { type } = event.data;
+    if (type === 'cancel') {
+        cancelled = true;
+        return;
+    }
+    if (type !== 'start') return;
+
+    cancelled = false;
+    const { build, monster, itemsById, conditionsById, locks, filtersBySlot, maxHpLossPerKill } = event.data;
+    const items = Object.values(itemsById);
+    const candidateLists = buildCandidateLists(items, locks, filtersBySlot);
+
+    const top10 = await searchBestBuilds(build, monster, { itemsById, conditionsById }, candidateLists, {
+        maxHpLossPerKill,
+        onProgress: ({ evaluated, total, top10 }) => {
+            self.postMessage({ type: 'progress', evaluated, total, top10 });
+        },
+        shouldCancel: () => cancelled,
+    });
+
+    self.postMessage({ type: 'done', top10 });
+};
